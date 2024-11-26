fcb367549ccce8bf6428d7c2ee89956fac5ceb36
eegrigorov <grigorov.us@gmail.com>
2024-11-27 00:56:38 +0300

    some refactoring

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
-import time
+from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    browser.implicitly_wait(10)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button == browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
